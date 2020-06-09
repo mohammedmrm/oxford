7c3543644abe8ec4979ce5ec509f6d509d98abeb
MohammedIT94 <mohammed.rbn4@yahoo.com>
2020-06-09 19:00:35 +0300

    pwa v1

--- re_sw.js
+++ re_sw.js
@@ -31,7 +31,7 @@ window.addEventListener('load', () => {
   });
   async function registerSW() {
   if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/albarq-pwa/sw.js")    .then(function(registration) {
+      navigator.serviceWorker.register("/sw.js")    .then(function(registration) {
           console.log("Service Worker registered with scope:", registration.scope);   
       }).catch(function(err) {     
           console.log("Service worker registration failed:", err);   
