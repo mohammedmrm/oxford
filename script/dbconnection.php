e5fb972a7065b062820e754f0a661c9cf5660797
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-07-04 19:25:54 +0300

    bugs fix

--- script/dbconnection.php
+++ script/dbconnection.php
@@ -2,8 +2,7 @@
 try{
 
 $con = new PDO('mysql:host=localhost;dbname=ox', "root",
-"root", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"));
-//$con->exec("SET CHARACTER SET UTF8");
+"", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"));
 $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException  $e ){
  echo "Error: ".$e;
