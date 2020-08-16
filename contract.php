a9af52d697dcee43b10fb51a2d884667019d14eb
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2020-08-17 00:58:08 +0300

    bugs

--- contract.php
+++ contract.php
@@ -1,187 +1,158 @@
 <?php
-
+ini_set('max_execution_time', 20000);
+ob_start();
 session_start();
 error_reporting(0);
+session_start();
 require_once("script/_access.php");
 access([1,2,4,3,5]);
 require_once("script/dbconnection.php");
 $id = $_REQUEST['id'];
 try{
-  $query = "select students.*, branches.name as branch_name,
+  $query = "select students.*,sum(if(payment.confirm = 0,payment.amount,0)) as remain, branches.name as branch_name,
              DATE_FORMAT(students.date,'%Y/%m/%d') as date,
-             levels.name as level_name,
+             levels.name as level_name,if(students.gender = 1,'ذكر','انثى') as gen,
              staff.name as staff_name,
              students.name as student_name
             from students
             left join branches on branches.id = students.branch_id
             left join levels on levels.id = students.level_id
             left join staff on staff.id = students.manager_id
-            where students.id = ?
+            left join payment on payment.student_id = students.id
+            where students.id = ? group by students.id
             ";
   $data = getData($con,$query,[$id]);
+  $data = $data[0];
   $success="1";
-  $student = $data[0];
 } catch(PDOException $ex) {
    $data=["error"=>$ex];
    $success="0";
 }
 
+require_once("tcpdf/tcpdf.php");
+class MYPDF extends TCPDF {
+    public function Header() {
 
-?>
+    }
+    public function Footer() {
+        // Set font
+        $this->SetFont('aealarabiya', 'B', 8);
+        // Title
+        $footer= '<hr>Developed By Mohammed Ridha - 07822816693';
+        $this->writeHTML($footer);
+        $this->writeHTML('');
+    }
+}
+$pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-<style>
-@media print {
-  body * :not(#section-to-print *){
-    visibility: hidden;
-    display: none;
-  }
-  #section-to-print, #section-to-print * {
-    visibility: visible;
-  }
-}
-/* arabic */
-@font-face {
-  font-family: 'Cairo';
-  font-style: normal;
-  font-weight: 400;
-  font-display: swap;
-  src: local('Cairo'), local('Cairo-Regular'), url(Cairofont.woff2) format('woff2');
-  unicode-range: U+0600-06FF, U+200C-200E, U+2010-2011, U+204F, U+2E41, U+FB50-FDFF, U+FE80-FEFC;
-}
-/* latin-ext */
-@font-face {
-  font-family: 'Cairo';
-  font-style: normal;
-  font-weight: 400;
-  font-display: swap;
-  src: local('Cairo'), local('Cairo-Regular'), url(Cairofont.woff2) format('woff2');
-  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-}
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('07822816693');
+$pdf->SetTitle('Registration Form');
+$pdf->SetSubject($data['name']);
+// set some language dependent data:
+$lg = Array();
+$lg['a_meta_charset'] = 'UTF-8';
+$lg['a_meta_dir'] = 'rtl';
+$lg['a_meta_language'] = 'ar';
+$lg['w_page'] = 'page';
 
-body * :not(.signture) {
-  font-family: 'Cairo', sans-serif !important;
-}
-body {
-  background-color:transparent;
-  font-size: 15px;
-  line-height: 1;
-  direction: rtl;
-}
+// set some language-dependent strings (optional)
+$pdf->setLanguageArray($lg);
+// set font
+$pdf->SetFont('aealarabiya', '', 12);
 
-.col-2 {
-  width:50%;
-  min-height: 3px;
-  float: left;
-  position: relative;
-  text-align: right;
-}
-.col-3 {
-  width:33.333333333%;
-  min-height: 3px;
-  float: left;
-  position: relative;
-  text-align: right;
-}
-.col-3 * {
-  padding-left: 10px;
-  padding-right: 10px;
-  text-align: center;
-}
+// set default header data
+//$pdf->SetHeaderData("../../../".$config['Company_logo'],35, "التقرير الشامل", "اسم");
+// set header and footer fonts
+$pdf->setHeaderFont(Array('aealarabiya', '', 12));
+$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 
-.col {
-  width:100%;
-  min-height: 3px;
-  float: left;
-  position: relative;
-  text-align: right;
-  border-bottom: 2px #CC0033 double;
-}
-.col-1 {
-  width:100%;
-  min-height: 3px;
-  float: left;
-  position: relative;
-  text-align: right;
-  line-height: 1.5;
-}
-.col-1 span {
-  color: #CC0000;
-  font-weight: bold;
-  padding-right: 15px;
-  padding-left: 15px;
-}
-.col-1 label,.col-1 pre {
-  color: #000;
-  font-weight:;
-  padding-right: 15px;
-  padding-left: 15px;
-  display: inline-block;
-}
-ul {
-  list-style: disc;
-}
 
-ul li {
-  line-height: 1.5;
-  padding-right: 15px;
-  padding-left: 15px;
-  margin-bottom:3px;
-}
+// set margins
+$pdf->SetMargins(10,5,10);
+$pdf->SetHeaderMargin(1);
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+// set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 
-.logo{
-  background-image: url(img/logos/logo.png);
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: contain;
-  height: 120px;
-}
+// set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
-.container {
-  width:800px;
-  background-color: #FFFFFF;
-  margin: auto;
-  position: absolute;
-  background-image: url(img/logos/bg-logo.png);
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: 70%;
-  border: 1px #888888 solid;
-  border-radius: 10px;
-}
-.print {
-  position: absolute;
-  left: 10;
-  width:200px;
-  margin: 0;
-  padding:10px;
-}
-.print button{
-  background-color: #33CC00;
-  border-radius: 10px;
-  outline: none;
-  width:100%;
-  color:#FFFFFF;
-  font-size:20px;
-}
 
-</style>
-<div class="print">
- <button type="button" onclick="print()">طباعة</button>
-</div>
-<div class="container" id="section-to-print">
-    <div class='col'>
-        <div class='col-3'>
-         <h2 style="color:#CC0033"><b>Oxford EST.</b></h2>
-         <h3 style="color:#555555">for language learning & Development</h3>
-        </div>
-        <div class='col-3 logo'></div>
 
-        <div class='col-3'>
-         <h2 style="color:#CC0033"><b>مؤسسة  اكسفورد</b></h2>
-         <h3 style="color:#555555" >الـــــــــــــــــــــدولية<br /> للغات والتطوير</h3>
-        </div>
-    </div>
-    <div class='col-2' dir="rtl">
+// ---------------------------------------------------------
+
+
+// add a page
+$pdf->AddPage($pageDir, 'A4');
+
+// Persian and English content
+$header=    '
+             <table>
+             <tr>
+                    <td style="text-align: center;font-size:20px;">
+                      <span style="color:#A52A2A;">Oxford EST.</span><br />
+                       for language learning & Development
+                    </td>
+                    <td style="text-align: center;">
+                        <img src="img/logos/logoW.jpg" height="100px"/>
+                    </td>
+                    <td style="text-align: center;font-size:20px;">
+                      <span style="color:#A52A2A;">مؤسسة اكسفورد </span><br /><br />
+                      الـــــــــــــــــــــدولية
+                      للغات والتطوير
+                    </td>
+             </tr>
+            </table>
+            <HR />
+            ';
+ $iqd = $data['total_price'] * 1250;
+$htmlpersian = '
+             <table>
+             <tr>
+                    <td>
+                        <span style="color:#CC0033;border-bottom: 1px solid #555555;">عقد تسجيل الطالب</span>
+                        <div >
+                         <span>هذا الاتفاق بين</span><br />
+                         <span>الطرف الاول : </span><label>مؤسسة اكسفورد التعليميه</label><br />
+                         <span>الطرف الثاني :</span><label>'.$data['name'].'</label><br />
+                         <span>رقم الطالب :</span><label>'.$data['student_number'].'</label><br /><br />
+                         <label>ينص العقد على ان الطرف الاول سوف يقوم بتعليم الطرف الثاني اللغة <u><b>الانكَلزية</b></u> وفقا للشروط و الاحكام التاليه:</label>
+                         <br /><br />
+                         <span style="color:#CC0033;">ساعات الدورة</span><br />
+                         <label>ايام الدورة التدريبية هي ثلاث او اربع ايام في الأسبوع بواقع 6 او 8  ساعات اسبوعيا .</label>
+                         <br />
+                         <span style="color:#CC0033;">الفترة</span><br />
+                         <label>تكون المدة المطلوبة لتمكن الطالب من التحدث اللغة  </label>
+                         <br />
+                         <span style="color:#CC0033;">مكان الدورة </span><br />
+                         <label>يكون موقع اعطاء دروس الدورة في موقع المؤسسة و حسب الرقعة الجغرافية للطالب .</label>
+                         <br />
+                         <span style="color:#CC0033;">اجور الدورة</span><br />
+                         <p>
+                          تحدد اجور الدورة الدراسية حسب الصنف الذي تم تصنيفه
+                          للطالب بعد اجتيازه امتحان تحديد المستوى عند التسجيل ,
+                          ليتم تحديد المستوى المناسب لقدرات الطالب <br />
+                          <br />
+                          تكون بثلاث مستويات.<br />
+                          <br />
+
+                          مبلغ الدورة '.$data['total_price'].' دولار امريكي اي ما يقارب '.$iqd.' دينار
+                          عراقي مع اجور استمارة التسجيل 20$ اي ما يقارب
+                          25000 دينار عراقي كحد ادنى و حسب قيمة الصرف
+                          آنذاك .
+                          اضافة الى 10$ اي ما يقارب 12500 دينار عراقي عند
+                          الانتقال من المستوى الاول الى المستوى الثاني .
+                         </p>
+                         <br />
+
+
+                        </div>
+
+                    </td>
+                    <td >
+<div>
       <span style="color:#CC0033;padding: 10px; border-bottom: 1px solid #555555;"><b>الالتزمات</b></span>
       <ul>
         <li> 	عدم استرجاع اي مبالغ مادية بعد توقيع هذا العقد حتى في حال عدم المباشرة .</li>
@@ -205,57 +176,52 @@ ul li {
         <li>	يمنح الطالب شهادة صادرة من اكاديمية اكسفورد في حالة اجتيازه الامتحان النهائي و في خلافه لايتم المنح</li>
       </ul>
     </div>
-    <div class='col-2'>
-        <span style="color:#CC0033;padding: 10px;border-bottom: 1px solid #555555;"><b>عقد تسجيل الطالب</b></span>
-      <div class="col-1"><br />
-       <span>هذا الاتفاق بين</span><br />
-       <span>الطرف الاول : </span><label>مؤسسة اكسفورد التعليميه</label><br />
-       <span>الطرف الثاني :</span><label><?php echo $student['student_name'];?></label><br />
-       <span>رقم الهوية :</span><label>N/A</label><br /><br />
-       <label>ينص العقد على ان الطرف الاول سوف يقوم بتعليم الطرف الثاني اللغة <u><b>الانكَلزية</b></u> وفقا للشروط و الاحكام التاليه:</label>
-       <br /><br />
-       <span>ساعات الدورة</span><br /><label>اٌام الدورة التدرٌبٌة هً ثالث او اربع اٌام فً األسبوع بواقع 6 او 8 ساعات اسبوعٌا .</label>
-       <br />
-       <span>الفترة</span><label>تكون المدة المطلوبة لتمكن الطالب من التحدث اللغة
-هً المدة الفعلٌة إلعطاء الدورة فً المؤسسة</label>
-       <br />
-       <span>مكان الدورة </span><br /><label>ٌكون موقع اعطاء دروس الدورة فً موقع المؤسسة و حسب الرقعة الجغرافٌة للطالب .</label>
-       <br />
-       <span>اجور الدورة</span><br />
-       <pre>
-تحدد اجور الدورة الدراسية حسب الصنف الذي تم تصنيفه
-للطالب بعد اجتيازه امتحان تحديد المستوى عند التسجيل ,
-ليتم تحديد المستوى المناسب لقدرات الطالب
-تكون بثلاث مستويات.
-مبلغ الدورة <?php echo $student['total_price'];?> دولار امريكي اي ما يقارب <?php echo  ($student['total_price'] * 1250); ?> دينار
-عراقي مع اجور استمارة التسجيل 20$ اي ما يقارب
-25000 دينار عراقي كحد ادنى و حسب قيمة الصرف
-آنذاك .
-اضافة الى 10$ اي ما يقارب 12500 دينار عراقي عند
-الانتقال من المستوى الاول الى المستوى الثاني .
-       </pre>
-       <br />
 
+                    </td>
 
-      </div>
-    </div>
-    <div class="col-1">
-      <div class="col-2">
-       <span>الطرف الاول </span>
+             </tr>
+            </table>
+' ;
+
+$htmlpersian .=
+'
+             <table>
+             <tr>
+                    <td style="text-align: center;">
+       <span style="color:#CC0033;">الطرف الثاني </span>
+       <br /><label>اسم الطالب : '.$data['student_name'].'</label>
+       <br /><label>التاريخ : '.$data['date'].'</label>
+       <br /><label>التوقيع: </label><label class="signture" style="font-family: \'Blackadder ITC\'"></label>
+
+                    </td>
+                    <td style="text-align: center;">
+       <span style="color:#CC0033;">الطرف الاول </span>
        <br /><label>مؤسسة اكسفورد التعليميه المتمثلة
 بالدكتور احمد الرماحي او من ينوب عنه</label>
-       <br /><label>التاريخ : <?php echo $student['date'];?></label>
-       <br /><label>التوقيع: </label><label class="signture" style="font-family: 'Blackadder ITC'"><?php echo "Ahmed Al-ramahee";?></label>
-      </div>
-      <div class="col-2">
-       <span>الطرف الثاني </span>
-       <br /><label>اسم الطالب : <?php echo $student['student_name'];?></label>
-       <br /><label>التاريخ : <?php echo $student['date'];?></label>
-       <br /><label>التوقيع: </label><label class="signture" style="font-family: 'Blackadder ITC'"><?php echo "Student";?></label>
-      </div>
-      <div class="col-1"><hr /> </div>
-    </div>
-</div>
-<script>
+       <br /><label>التاريخ : '.$data['date'].'</label>
+       <br /><label>التوقيع: </label><label class="signture" style="font-family: \'Blackadder ITC\'"></label>
 
-</script>
+                    </td>
+
+             </tr>
+            </table>
+';
+$pdf->setRTL(Ture);
+$pdf->SetFontSize(12);
+$pdf->WriteHTML($style.$header.$htmlpersian, true, false, true, false, 'J');
+
+// embed image
+$pdf->Image('img/logos/bg-logo.png', 40, 100, 100, 100, '', '', '', false, 200, '', false, false, 0);
+// set LTR direction for english translation
+$pdf->setRTL(false);
+
+$pdf->SetFontSize(10);
+// print newline
+$pdf->Ln();
+//Close and output PDF document
+ob_end_clean();
+
+$pdf->Output('RegistrationForm '.date('Y-m-d h:i:s').'.pdf', 'I');
+
+?>
+
